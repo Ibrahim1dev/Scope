701af6541051d3efc2e0ce9e6a20b97581c61717
Ibrahim <ahmitisalih@Ahmitis-MBP.lan>
2018-03-17 18:24:17 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+//SCOPE
+//Global variables
+var student1 = "Abel";
+var student2 = "Hana";
+var student3 = "Malu";
+
+//can access from every where
+//TEST 1 Global variabels
+console.log("Result: " + student1 + " < " + student2 + " < "+ student3); //Result: Abel < Hana < Malu
+
+//Test 2
+function isClever(stud){
+  student4 = "John";
+  console.log(stud + " is the cleveriest");
+}
+
+isClever(student2); // Hana is the cleveriest
+isClever(student4); // John is the cleveriest
+
+
+//RETURN
+// In function we can use return instead of console.log();
+// use console.log();
+function addNumbers(num1, num2){
+  console.log(num1 + num2);
+}
+
+addNumbers(3, 5);
+
+//Using return
+function addNumbers1(number1, number2){
+  return number1 + number2;
+}
+
+addNumbers1(10, 10);
+
+
